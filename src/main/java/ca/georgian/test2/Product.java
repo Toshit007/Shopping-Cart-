6ba14f8e0aef840f5ec37a78f3b0f24383008ca6
Toshit Narwal <toshitnarwal@gmail.com>
2024-08-15 15:23:42 -0400

    Question 2 complete

--- src/main/java/ca/georgian/test2/Product.java
+++ src/main/java/ca/georgian/test2/Product.java
@@ -1,6 +1,16 @@
 package ca.georgian.test2;
 
 public class Product {
+    private int id;
+    private String sku;
+    private String name;
+    private double salePrice;
+    private double regularPrice;
+    private String image;  // URL for image
 
+    @Override
+    public String toString() {
+        return name + "-$" + salePrice;
+    }
 
 }
